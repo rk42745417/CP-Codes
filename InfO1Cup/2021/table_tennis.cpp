4ecf0c36ce67280e73c4cddc57281fc5209361f5
rk42745417 <rk427454171@gmail.com>
2021-10-21 15:17:15 +0800

    Fix InfO1 2021 pA

--- InfO1Cup/2021/table_tennis.cpp
+++ InfO1Cup/2021/table_tennis.cpp
@@ -25,33 +25,53 @@ signed main() {
 	const int m = n + k;
 	vector<int> ans;
 	ans.reserve(n);
-	for(int i = 0; i <= k; i++) {
-		for(int j = 0; j + i <= k; j++) {
-			int s = arr[i] + arr[m - j - 1];
-			int rest = k - j - i;
-			int l = i, r = m - j - 1;
-			while(ans.size() < n && l < r) {
-				if(arr[l] + arr[r] == s) {
-					ans.push_back(arr[l]);
-					ans.push_back(arr[r]);
-					l++;
-					r--;
+	auto run = [&](int s) {
+		int rest = k, l = 0, r = m - 1;
+		while(rest >= 0 && ans.size() < n) {
+			if(arr[l] + arr[r] == s) {
+				ans.push_back(arr[l]);
+				ans.push_back(arr[r]);
+				l++;
+				r--;
+			}
+			else if(arr[l] + arr[r] > s)
+				rest--, r--;
+			else
+				rest--, l++;
+		}
+	};
+	if(m <= 4 * k) {
+		for(int i = 0; i <= k; i++) {
+			for(int j = 0; j + i <= k; j++) {
+				run(arr[i] + arr[m - j - 1]);
+				if(ans.size() == n) {
+					sort(ans.begin(), ans.end());
+					for(int a : ans)
+						cout << a << ' ';
+					cout << '\n';
+					return 0;
 				}
-				else if(arr[l] + arr[r] > s)
-					rest--, r--;
-				else
-					rest--, l++;
-				if(rest < 0)
-					break;
+				ans.clear();
 			}
-			if(ans.size() == n) {
-				sort(ans.begin(), ans.end());
-				for(int a : ans)
-					cout << a << ' ';
-				cout << '\n';
-				return 0;
+		}
+	}
+	else {
+		map<int, int> cnt;
+		for(int i = 0; i < 2 * k; i++)
+			for(int j = 0; j < 2 * k; j++)
+				cnt[arr[i] + arr[m - j - 1]]++;
+		for(const auto &[s, c] : cnt) {
+			if(c >= k) {
+				run(s);
+				if(ans.size() == n) {
+					sort(ans.begin(), ans.end());
+					for(int a : ans)
+						cout << a << ' ';
+					cout << '\n';
+					return 0;
+				}
+				ans.clear();
 			}
-			ans.clear();
 		}
 	}
 }
