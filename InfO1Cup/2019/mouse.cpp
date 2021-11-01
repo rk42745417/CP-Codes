aaa29926a816178f5f2a7479a9819af5c234f46f
rk42745417 <rk427454171@gmail.com>
2021-11-01 12:21:35 +0800

    Add info1 2019 mouse full score

--- InfO1Cup/2019/mouse.cpp
+++ InfO1Cup/2019/mouse.cpp
@@ -1,4 +1,4 @@
-// 34/100
+// 100/100
 #include "grader.h"
 #include <bits/stdc++.h>
 using namespace std;
@@ -17,89 +17,112 @@ const double EPS = 1e-8;
 
 void solve(int n) {
 	vector<int> arr(n);
-	vector<bool> fixed(n);
 	for(int i = 0; i < n; i++)
 		arr[i] = i + 1;
-	for(int i = 0; i < n; i++) {
-		if(fixed[i])
-			continue;
+	mt19937 rnd(time(0));
+	while(true) {
 		int w = query(arr);
 		if(w == n)
 			return;
-		bool ok = 0;
-
-		int tot = 0, neg = 0;
-		int a = -1, b = -1;
-
-		for(int j = 0; j < n; j++) {
-			if(i == j)
-				continue;
-			swap(arr[i], arr[j]);
-			int x = query(arr);
-			swap(arr[i], arr[j]);
-			if(x == n)
-				return;
-
-			if(fixed[j]) {
-				if(x - w == -2) {
-					fixed[i] = true;
-					ok = 1;
-					break;
-				}
-				continue;
-			}
-			tot++;
-
-			if(x - w == 2) {
-				swap(arr[i], arr[j]);
-				fixed[i] = fixed[j] = true;
-				ok = 1;
-				break;
-			}
-			if(x - w == 1) {
-				if(a == -1)
-					a = j;
-				else if(b == -1)
-					b = j;
-				else
-					assert(false);
-			}
-			if(x - w == -1) {
-				neg++;
-			}
-			if(x - w == -2) {
-				fixed[i] = fixed[j] = true;
-				ok = 1;
-				break;
-			}
-		}
-		if(ok)
-			continue;
-		if(tot == neg)
-			fixed[i] = true;
-		assert(b != -1);
-		swap(arr[i], arr[a]);
-		swap(arr[i], arr[b]);
-		int x = query(arr);
-		if(x == n)
+		if(w == 0)
+			break;
+		shuffle(arr.begin(), arr.end(), rnd);
+	}
+	vector<pair<int, int>> valid;
+	auto my_query = [&](vector<pair<int, int>> &owo) {
+		for(const auto &[a, b] : owo)
+			swap(arr[a], arr[b]);
+		int w = query(arr);
+		for(const auto &[a, b] : owo)
+			swap(arr[a], arr[b]);
+		return w;
+	};
+	bool flag = false;
+	function<void(int, int, vector<pair<int, int>>&, int)> divide = [&](int l, int r, vector<pair<int, int>> &pairs, int sum) {
+		if(sum == 0 || flag)
 			return;
-		if(x - w >= 2) {
-			fixed[i] = fixed[a] = true;
-			if(x - w == 3)
-				fixed[b] = true;
+		if(l == r - 1) {
+			valid.push_back(pairs[l]);
+			return;
+		}
+		vector<pair<int, int>> xdd;
+		int m = l + r >> 1;
+		for(int i = l; i < m; i++)
+			xdd.push_back(pairs[i]);
+		int ls = my_query(xdd), rs = sum - ls;
+		if(ls == n)
+			flag = true;
+		divide(l, m, pairs, ls);
+		divide(m, r, pairs, rs);
+	};
+	for(int i = 1; i <= n; i++) {
+		vector<pair<int, int>> has;
+		for(int j = 0; j < n; j++) {
+			int k = (i - j + n) % n;
+			if(j < k)
+				has.push_back({j, k});
+		}
+		divide(0, has.size(), has, my_query(has));
+		if(flag)
+			return;
+	}
+	assert(!valid.empty() && valid.size() <= n);
+	//cerr << "collected all pairs: ";
+	//for(const auto &[a, b] : valid)
+		//cerr << a << ' ' << b << '\n';
+	//cerr << "----------------\n";
+	vector<vector<int>> edge(n);
+	for(const auto &[a, b] : valid) {
+		edge[a].push_back(b);
+		edge[b].push_back(a);
+	}
+	vector<bool> vis(n);
+	function<void(int, vector<int>&)> dfs = [&](int u, vector<int>&tra) {
+		vis[u] = true;
+		tra.push_back(u);
+		for(int v : edge[u])
+			if(!vis[v])
+				dfs(v, tra);
+	};
+	vector<bool> owo;
+	vector<vector<int>> travs;
+	vector<int> ans = arr;
+	for(int i = 0; i < n; i++) {
+		if(vis[i])
+			continue;
+		vector<int> tra;
+		dfs(i, tra);
+		//cerr << i << "-th traversal: ";
+		//for(int a : tra)
+			//cerr << a << ' ';
+		//cerr << '\n';
+		for(int i = 0; i < tra.size(); i++) {
+			ans[tra[i]] = arr[tra[(i + 1) % tra.size()]];
+		}
+		int w = query(ans);
+		if(w == n)
+			return;
+		if(w > 0)
+			owo.push_back(true);
+		else
+			owo.push_back(false);
+		travs.push_back(tra);
+		ans = arr;
+	}
+	for(int i = 0; i < owo.size(); i++) {
+		if(owo[i]) {
+			auto &tra = travs[i];
+			for(int j = 0; j < tra.size(); j++) {
+				ans[tra[j]] = arr[tra[(j + 1) % tra.size()]];
+			}
 		}
 		else {
-			swap(arr[i], arr[b]);
-			swap(arr[i], arr[a]);
-			swap(arr[i], arr[b]);
-			swap(arr[i], arr[a]);
-			fixed[i] = fixed[b] = true;
-			if(x - w == 1)
-				fixed[a] = true;
+			auto &tra = travs[i];
+			for(int j = 0; j < tra.size(); j++)
+				ans[tra[(j + 1) % tra.size()]] = arr[tra[j]];
 		}
-		//cout << "owo " << a << ' ' << b << '\n';;
 	}
-	query(arr);
+	assert(query(ans) == n);
 }
 
 /*
