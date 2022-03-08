a4a0c3a55b411c0f2af53273a8468ecff6bfeffa
rk42745417 <rk427454171@gmail.com>
2022-03-08 14:17:10 +0800

    JOI Final 2018 pE full

--- JOI/Final Round/2018/pE.cpp
+++ JOI/Final Round/2018/pE.cpp
@@ -1,61 +1,106 @@
-// 22/100
+// 100/100
+/*
+--------------              |   /
+      |                     |  /
+      |                     | /
+      |             *       |/          |    |         ------            *
+      |                     |           |    |        /      \
+      |             |       |\          |    |       |       |\          |
+   \  |             |       | \         |    |       |       | \         |
+    \ |             |       |  \        |    |        \     /   \        |
+     V              |       |   \        \__/|         -----     \       |
+*/
 #include <bits/stdc++.h>
 using namespace std;
+#define debug(x) cerr << "\e[1;31m" << #x << " = " << (x) << "\e[0m\n"
+#define print(x) emilia_mata_tenshi(#x, begin(x), end(x))
+template<typename T> void emilia_mata_tenshi(const char *s, T l, T r) {
+	cerr << "\e[1;33m" << s << " = [";
+	while(l != r) {
+		cerr << *l;
+		cerr << (++l == r ? ']' : ',');
+	}
+	cerr << "\e[0m\n";
+}
 
-#define EmiliaMyWife ios::sync_with_stdio(0); cin.tie(0);
+#define EmiliaMyWife ios::sync_with_stdio(0); cin.tie(NULL);
 using ll = int64_t;
-using uint = uint32_t;
 using ull = uint64_t;
 using ld = long double;
-const int INF = 0x3f3f3f3f;
-const int MOD = 1e9 + 7;
-const double EPS = 1e-8;
-const ll LINF = ll(4e18) + ll(2e15);
-static int LamyIsCute = []() {
+using uint = uint32_t;
+const double EPS  = 1e-8;
+const int INF     = 0x3F3F3F3F;
+const ll LINF     = 4611686018427387903;
+const int MOD     = 1e9+7;
+static int Lamy_is_cute = []() {
 	EmiliaMyWife
 	return 48763;
 }();
+/*--------------------------------------------------------------------------------------*/
+
+const int N = 2e6 + 25;
+int arr[N], dp[N], dp2[N], l;
+int dfs(const string &s, int n, int cur) {
+	if(n == l)
+		return arr[cur];
+	if(s[n] == '?')
+		return dfs(s, n + 1, cur) + dfs(s, n + 1, cur | (1 << n));
+	if(s[n] == '1')
+		return dfs(s, n + 1, cur | (1 << n));
+	return dfs(s, n + 1, cur);
+}
+int dfs2(const string &s, int n, int cur, int cnt) {
+	if(n == l)
+		return dp[cur] * (cnt % 2 ? -1 : 1);
+	if(s[n] == '?')
+		return dfs2(s, n + 1, cur | (1 << n), cnt);
+	if(s[n] == '0')
+		return dfs2(s, n + 1, cur, cnt);
+	return dfs2(s, n + 1, cur | (1 << n), cnt) + dfs2(s, n + 1, cur, cnt + 1);
+}
+int dfs3(const string &s, int n, int cur, int cnt) {
+	if(n == l)
+		return dp2[cur] * (cnt % 2 ? -1 : 1);
+	if(s[n] == '?')
+		return dfs3(s, n + 1, cur, cnt);
+	if(s[n] == '1')
+		return dfs3(s, n + 1, cur | (1 << n), cnt);
+	return dfs3(s, n + 1, cur | (1 << n), cnt + 1) + dfs3(s, n + 1, cur, cnt);
+}
 
 signed main() {
-	int n, q;
-	cin >> n >> q;
-	vector<int> mpow(n + 1, 1);
-	for(int i = 1; i <= n; i++)
-		mpow[i] = mpow[i - 1] * 3;
-	vector<int> dp(mpow[n]), prv(mpow[n]);
+	int q;
+	cin >> l >> q;
 	{
 		string s;
 		cin >> s;
-		for(int i = 0; i < s.size(); i++) {
-			int r = 0;
-			for(int j = 0; j < n; j++)
-				if(i >> j & 1)
-					r += mpow[j];
-			dp[r] = s[i] - '0';
-		}
+		for(int i = 0; i < (1 << l); i++)
+			arr[i] = dp[i] = dp2[i] = s[i] - '0';
 	}
-	for(int i = 0; i < n; i++) {
-		swap(prv, dp);
-		for(int j = 0; j < mpow[n]; j++) {
-			int x = j / mpow[i] % 3;
-			if(x < 2)
-				dp[j] = prv[j];
-			else
-				dp[j] = prv[j - mpow[i] * 2] + prv[j - mpow[i]];
-		}
-	}
+	for(int i = 0; i < l; i++)
+		for(int j = 0; j < (1 << l); j++)
+			if(j >> i & 1)
+				dp[j] += dp[j ^ (1 << i)];
+	for(int i = 0; i < l; i++)
+		for(int j = 0; j < (1 << l); j++)
+			if(j >> i & 1 ^ 1)
+				dp2[j] += dp2[j ^ (1 << i)];
 
 	while(q--) {
 		string s;
 		cin >> s;
 		reverse(s.begin(), s.end());
-		int r = 0;
-		for(int i = 0; i < n; i++) {
-			if(s[i] == '?')
-				r += mpow[i] * 2;
-			else
-				r += mpow[i] * (s[i] & 1);
+		int cnt = 0, cnt2 = 0;
+		for(char c : s) {
+			cnt += c == '?';
+			cnt2 += c == '1';
 		}
-		cout << dp[r] << '\n';
+		int cnt3 = l - (cnt + cnt2);
+		if(cnt <= min(cnt3, cnt2))
+			cout << dfs(s, 0, 0) << '\n';
+		else if(cnt2 <= min(cnt, cnt3))
+			cout << dfs2(s, 0, 0, 0) << '\n';
+		else
+			cout << dfs3(s, 0, 0, 0) << '\n';
 	}
 }
