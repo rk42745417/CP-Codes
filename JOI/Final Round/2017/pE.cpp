aaccd603933267584effe4e4c37bda087c9875f8
rk42745417 <rk427454171@gmail.com>
2022-03-09 08:05:54 +0800

    JOI Final 2017 pE full

--- JOI/Final Round/2017/pE.cpp
+++ JOI/Final Round/2017/pE.cpp
@@ -1,47 +1,82 @@
-// 0/100
+// 100/100
+/*
+--------------              |   /
+      |                     |  /
+      |                     | /
+      |             *       |/          |    |         ------            *
+      |                     |           |    |        /      \
+      |             |       |\          |    |       |       |\          |
+   \  |             |       | \         |    |       |       | \         |
+    \ |             |       |  \        |    |        \     /   \        |
+     V              |       |   \        \__/|         -----     \       |
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+#define debug(x) cerr << "\e[1;31m" << #x << " = " << (x) << "\e[0m\n"
+#define print(x) emilia_mata_tenshi(#x, begin(x), end(x))
+template<typename T> void emilia_mata_tenshi(const char *s, T l, T r) {
+	cerr << "\e[1;33m" << s << " = [";
+	while(l != r) {
+		cerr << *l;
+		cerr << (++l == r ? ']' : ',');
+	}
+	cerr << "\e[0m\n";
+}
 
-#define EmiliaMyWife ios::sync_with_stdio(0); cin.tie(0);
+#define EmiliaMyWife ios::sync_with_stdio(0); cin.tie(NULL);
 using ll = int64_t;
 using ull = uint64_t;
-using uint = uint32_t;
 using ld = long double;
-const int INF = 0x3f3f3f3f;
-const int MOD = 1e9 + 7;
-const ll LINF = ll(4e48) + ll(2e15);
-const double EPS = 1e-9;
-static int LamyIsCute = []() {
+using uint = uint32_t;
+const double EPS  = 1e-8;
+const int INF     = 0x3F3F3F3F;
+const ll LINF     = 4611686018427387903;
+const int MOD     = 1e9+7;
+static int Lamy_is_cute = []() {
 	EmiliaMyWife
-	return 48763;
+		return 48763;
 }();
+/*--------------------------------------------------------------------------------------*/
 
 signed main() {
 	int n, m;
 	cin >> n >> m;
-	vector<int> arr(n), ans(m, INF);
-	for(int i = 0; i < n; i++)
-		cin >> arr[i], arr[i]--;
+	vector<int> arr(n);
+	for(int &a : arr)
+		cin >> a;
+	vector<int> ans(m + 1, INF);
 
-	for(int i = 0; i < m; i++)
-		for(int j = 0; j < m; j++) {
-			for(int k = 0; k < (1 << n); k++) {
-				vector<int> owo(n);
-				for(int w = 0; w < n; w++) {
-					if(k >> i & 1)
-						owo[w] = i;
-					else
-						owo[w] = j;
-				}
-				vector<vector<bool>> dp(n, vector<bool>(n));
-				for(int r = 1; r < n; r++) {
-					dp[r - ][r] = true;
-					for(int l = r - 2; ~l; l--) {
-					}
-				}
+	vector<pair<int, int>> cnt(m + 1);
+	vector<int> owo(m + 1);
+	for(int i = 1; i <= m; i++)
+		cnt[i].second = i;
+	for(int a : arr)
+		cnt[a].first++, owo[a]++;
+	sort(cnt.begin(), cnt.end(), greater<>());
+
+	for(int st : {1, 2}) {
+		vector<map<int, int>> adj(m + 1);
+		for(int i = st; i < n; i += 2) {
+			int a = arr[i - 1], b = arr[i];
+			if(a != b) {
+				adj[a][b]++;
+				adj[b][a]++;
 			}
 		}
 
-	for(int i = 0; i < m; i++)
+		for(int i = 1; i <= m; i++) {
+			int res = 0;
+			for(const auto &[c, j] : cnt) {
+				if(j == i)
+					continue;
+				res = max(res, c - adj[i][j]);
+				if(adj[i][j] == 0)
+					break;
+			}
+			ans[i] = min(ans[i], n - (res + owo[i]));
+		}
+	}
+	for(int i = 1; i <= m; i++)
 		cout << ans[i] << '\n';
 }
